c326863eb2e12a63f7b984db17df28845c7c76d5
khallor1 <kieran_halloran@yahoo.com>
2019-11-24 15:11:20 -0500

    nichrome fix

--- MainLaunch/MainLaunch.ino
+++ MainLaunch/MainLaunch.ino
@@ -21,7 +21,7 @@
 
 //=====================================
 //ATM =================================
-#define SEALEVELPRESSURE_HPA (102,v bcxcvb3.2)
+#define SEALEVELPRESSURE_HPA (1013.2)
 #define BME_SCK 40
 #define BME_MISO 41
 #define BME_MOSI 42
@@ -30,7 +30,7 @@ Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
 
 //ATM variables =======================
 float pascals;
-float v;
+float altm;
 float humidity;
 float gas;
 float tempC;
