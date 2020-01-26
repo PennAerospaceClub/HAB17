7abadb0d91be09599008ecca66e97669c174fc8b
Kieran Halloran <36603091+khallor1@users.noreply.github.com>
2020-01-26 14:58:52 -0500

    Send temp in rockblock instead of course

--- MainLaunch/SD.ino
+++ MainLaunch/SD.ino
@@ -37,9 +37,9 @@ String rbData(){
   String data = 
   String(latit, 6) + "," + 
   String(longit, 6) + "," + 
-  String(altm, 6) + "," + 
-  String(course, 2) + "," + 
-  String(speed, 2) + "," + 
+  String(altm, 6) + "," +  
+  String(speed, 2) + "," +
+  String(tempC, 2) + "," +
   String(nichromeOn, 1);
   
   return data;
